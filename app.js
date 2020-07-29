a3670e98d9344ddec3d9689ad595dc467a210fc2
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-07-29 15:57:26 -0400

    In root route, created conditional to check for items in array and render to page

--- app.js
+++ app.js
@@ -3,57 +3,61 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // npm install mongoose and require it
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // sets the connection for mongoDB and mongoose
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // creates schema of whatever thing you want
 const itemsSchema = {
   name: String
-}
+};
 // creates the model for the schema
-const Item = mongoose.model("Item", itemsSchema)
+const Item = mongoose.model("Item", itemsSchema);
 
 // created new items to save to DB
 const walkDog = new Item({
   name: "Walk the dog"
-})
+});
 
 const cookDinner = new Item({
   name: "Cook Dinner"
-})
+});
 
 const foldLaundry = new Item({
   name: "Fold the Laundry"
-})
+});
 
-const defaultItems = [walkDog, cookDinner, foldLaundry]
-
-// Item.insertMany(defaultItems, function(err){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     console.log("Successfully inserted Items");
-//   }
-// })
+const defaultItems = [walkDog, cookDinner, foldLaundry];
 
 app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully inserted Items");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    }
+  });
+});
 
-  Item.find({},function(err, items){
-        res.render("list", {listTitle: "Today", newListItems: items});
-      })
-    })
-
-app.post("/", function(req, res){
-
+app.post("/", function(req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
@@ -65,11 +69,11 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function(req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
